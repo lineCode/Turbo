eb6f7cd404b019f4c82f4a7f83cf65a57847c8fa
styinx <zornchrisjo@googlemail.com>
2018-06-26 21:27:52 +0200

    remove unused include

--- include/gui/text_widget.h
+++ include/gui/text_widget.h
@@ -1,26 +1,29 @@
 #ifndef TURBO_TEXT_WIDGET_H
 #define TURBO_TEXT_WIDGET_H
 
-#include "video/font.h"
-
 #include "gui/widget.h"
 
 namespace TURBO
 {
     namespace GUI
     {
-        class TextWidget : public Widget
+        class TextWidget
+            : public Widget
         {
         protected:
-            std::string text;
+            std::string    text;
             VIDEO::Texture *text_texture;
-            MATH::Rect text_rect;
+            MATH::Rect     text_rect;
 
         public:
             TextWidget(Object *parent, std::string text);
+
             ~TextWidget() override;
+
             std::string getText() const;
+
             std::string setText(std::string text);
+
             void draw(VIDEO::Renderer *renderer) override;
         };
     }
