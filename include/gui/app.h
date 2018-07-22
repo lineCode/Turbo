d4fd1c63268133a64c5e49db583809259e98e972
styinx <zornchrisjo@googlemail.com>
2018-07-22 14:07:24 +0200

    update app class

--- include/gui/app.h
+++ include/gui/app.h
@@ -10,18 +10,28 @@ namespace TURBO
         class App
         {
         private:
-            VIDEO::Window window;
-            VIDEO::Renderer renderer;
-            MainWidget main_widget;
+            SYSTEM::SDL     sdl;
+            VIDEO::Window   *window;
+            VIDEO::Renderer *renderer;
+            MainWidget      *main_widget;
 
         public:
             App(std::string app_name, MATH::Rect size, Uint32 window_flags = SDL_WINDOW_SHOWN,
                 Uint32 renderer_flags = SDL_RENDERER_SOFTWARE);
-            VIDEO::Window& getWindow();
-            VIDEO::Renderer& getRenderer();
-            MainWidget& getMainWidget();
+
+            ~App();
+
+            SYSTEM::SDL &getSDL();
+
+            VIDEO::Window &getWindow();
+
+            VIDEO::Renderer &getRenderer();
+
+            MainWidget &getMainWidget();
+
             int run();
-            void attach(Widget &widget);
+
+            void attach(Widget *widget);
         };
     }
 }
--- source/gui/app.cpp
+++ source/gui/app.cpp
@@ -5,26 +5,40 @@ namespace TURBO
     namespace GUI
     {
         App::App(std::string app_name, MATH::Rect size, Uint32 window_flags, Uint32 renderer_flags)
-            : window(app_name, size, window_flags),
-              renderer(window, -1, renderer_flags),
-              main_widget(window, renderer)
         {
+            sdl.initSDL(SDL_INIT_EVERYTHING);
+            sdl.initTTF();
 
+            window = new VIDEO::Window(app_name, size, window_flags);
+            renderer = new VIDEO::Renderer(*window, -1, renderer_flags);
+            main_widget = new MainWidget(*window, *renderer);
+        }
+
+        App::~App()
+        {
+            delete window;
+            delete renderer;
+            delete main_widget;
+        }
+
+        SYSTEM::SDL &App::getSDL()
+        {
+            return sdl;
         }
 
         VIDEO::Window &App::getWindow()
         {
-            return window;
+            return *window;
         }
 
         VIDEO::Renderer &App::getRenderer()
         {
-            return renderer;
+            return *renderer;
         }
 
         MainWidget &App::getMainWidget()
         {
-            return main_widget;
+            return *main_widget;
         }
 
         int App::run()
@@ -36,25 +50,25 @@ namespace TURBO
                 {
                     INPUT::Mouse::pollEvent(event);
                     INPUT::Keyboard::pollEvent(event);
-                    main_widget.pollEvent(event);
+                    main_widget->pollEvent(event);
 
                     if(event.type == SDL_QUIT || INPUT::Keyboard::pressed(SDLK_ESCAPE, KMOD_LCTRL))
                     {
                         SYSTEM::SYSTEM_RUNNING = false;
                     }
 
-                    renderer.clear();
-                    main_widget.draw(&renderer);
-                    renderer.present();
+                    renderer->clear();
+                    main_widget->draw(renderer);
+                    renderer->present();
                 }
                 SYSTEM::Clock::sleep(1);
             }
             return 0;
         }
 
-        void App::attach(Widget &widget)
+        void App::attach(Widget *widget)
         {
-            main_widget.setChild(&widget);
+            main_widget->setChild(widget);
         }
     }
 }
